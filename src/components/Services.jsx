ca6874523f1b3b4f2cb1937375f7104618ce6903
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-11-07 16:16:53 +0100

    Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import frontDeveloper from '../assets/loper.lottie';
-import frontloper from '../assets/mhoyinsk.lottie
+import frontloper from '../assets/mhoyinsk.lottie'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
